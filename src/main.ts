09fbeb8ce8db33284c8319e159d0171f42717e2a
kittimas <kittimas.ma@astartechs.com>
2025-12-24 10:31:10 +0700

    feat: add Swagger documentation and setup for API

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,40 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import * as path from 'path';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  app.setGlobalPrefix('/api');
+  app.setGlobalPrefix('api');
   app.use('/uploads', express.static(path.join(process.cwd(), 'public', 'uploads')));
 
-  await app.listen(process.env.PORT ?? 8000);
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API docs')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        in: 'header',
+        name: 'Authorization',
+      },
+      'bearerAuth',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/swagger', app, document);
+
+  const port = process.env.PORT ?? 8000;
+  await app.listen(port, '0.0.0.0');
+
+  
+  console.log(`API: http://localhost:${port}/api`);
+  console.log(`Swagger: http://localhost:${port}/api/swagger`);
 }
 bootstrap();
